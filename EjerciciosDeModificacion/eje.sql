15d94ff601513a734e3b76d9e525d97ec32729dc
avendanoangeline77 <uba.avendanoangelina@gmail.com>
2024-05-28 16:52:04 -0300

    termine los ejs

--- EjerciciosDeModificacion/eje.sql
+++ EjerciciosDeModificacion/eje.sql
@@ -1,14 +1,22 @@
+---Insert(from)---
 INSERT INTO artists(name)
 VALUES ("enrique iglesia")
 
 INSERT INTO albums(Title,ArtistId)
 VALUES ("Quizas", 276)
 --3 canciones de ese artista--
-insert INTO tracks(name,MediaTypeId,Milliseconds,UnitPrice)
+INSERT INTO tracks(name,MediaTypeId,Milliseconds,UnitPrice)
 VALUES ("El perdedor",1, 1200, 2.8 ), ("Lloro por ti", 1, 1000, 2.5), ("Dile que", 1 , 2000, 2.3) 
 
---------------------------------
-update tracks 
+---Update(nombre de la tabla)---
+UPDATE tracks 
 set MediaTypeId = 1
-where TrackId > 3500
+WHERE TrackId > 3500
 
+---Delete(from)----
+--borra un bloque entero-- 
+--cada vez que hacemos una consulta ; al final--
+--los id tienen valores unicos--
+DELETE FROM tracks where TrackId = 3504 or TrackId = 3505 or TrackId = 3506;
+DELETE FROM albums where AlbumId = 348;
+DELETE FROM artists where ArtistId = 276;
